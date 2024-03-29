a8d3d1460be142b8570f924115ba0878e60a9275
kkkatwr <Katyad1318@gmail.com>
2024-03-29 21:56:26 +0500

    get

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int TicketPrice = 35600; //стоимость билета
-        int MilesPrice = 20; // рублей за 1 милю
-        int Miles = TicketPrice / MilesPrice;
-        System.out.println("количество начисленных бонусных миль " + "= " + Miles);
+        int ticketPrice = 35600; //стоимость билета
+        int milesPrice = 20; // рублей за 1 милю
+        int miles = ticketPrice / milesPrice;
+        System.out.println("количество начисленных бонусных миль " + "= " + miles);
 
 
-    }
-}
+    }}
